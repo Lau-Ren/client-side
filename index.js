f11adf1a678a7dfd1b8c9af1bf93aebbf612291d
Lauren Colls & Nick Jacobs <pair+lau-ren.nick-jacobs@devbootcamp.com>
2016-05-23 10:41:51 +1200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+xhr.get('https://api.wheretheiss.at/v1/coordinates/37.795517,-122.393693', function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+  document.body.innerHTML = example({ name: data.body.map_url });
 })
 
