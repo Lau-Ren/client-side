0489d41ec9b6cb029956e1f869b6a18290286a4e
Lauren Colls & Nick Jacobs <pair+lau-ren.nick-jacobs@devbootcamp.com>
2016-05-23 10:59:47 +1200

    Updates

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var example = require('./views/example.hbs')
 xhr.get('https://api.wheretheiss.at/v1/coordinates/37.795517,-122.393693', function(err, data) {
   if (err) console.log(err) // do something
 
-  console.log(data.body.map_url)
-console.log("im updating1")
-  document.body.innerHTML = example({ name: data.body.map_url });
+  console.log(JSON.parse(data.body).map_url)
+  console.log("im updating1")
+  document.body.innerHTML = example({ url: data.body.map_url });
 })
 
