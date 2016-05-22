7c1cf4086d4dcfa5237ecc9f532b3ba32a565b33
Lauren Colls & Nick Jacobs <pair+lau-ren.nick-jacobs@devbootcamp.com>
2016-05-23 11:40:42 +1200

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('localhost:3000/v1/colours', function(err, data) {
+xhr.get('https://api.wheretheiss.at/v1/satellites/25544/positions', function(err, data) {
   if (err) console.log(err) // do something
-  var red = JSON.parse(data[0].name)
-  console.log(red)
-  console.log("colours")
-  document.body.innerHTML = colours({ red: red });
+  var sat = JSON.parse(data.body)
+  console.log(sat)
+  console.log("sat positions")
+  document.body.innerHTML = sat({ sat: sat });
 })
 
