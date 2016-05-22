54c8d8ce0d558c221e3942798a88c904db54c57c
Lauren Colls & Nick Jacobs <pair+lau-ren.nick-jacobs@devbootcamp.com>
2016-05-23 10:43:36 +1200

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var example = require('./views/example.hbs')
 xhr.get('https://api.wheretheiss.at/v1/coordinates/37.795517,-122.393693', function(err, data) {
   if (err) console.log(err) // do something
 
-  console.log(data.body)
+  console.log(data.body.map_url)
   document.body.innerHTML = example({ name: data.body.map_url });
 })
 
