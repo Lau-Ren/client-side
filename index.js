e39564de30f9c5225c520db90ba0500235f2cb65
Lauren Colls & Nick Jacobs <pair+lau-ren.nick-jacobs@devbootcamp.com>
2016-05-23 11:43:51 +1200

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var xhr = require('xhr')
 var sat = require('./views/sat.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites/25544/positions', function(err, data) {
+xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
   if (err) console.log(err) // do something
   var sat = JSON.parse(data.body)
   console.log(sat)
